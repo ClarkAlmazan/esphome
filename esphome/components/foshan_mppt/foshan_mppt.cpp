6086d275df87e7db329f909c68a3171db45c417a
ClarkAlmazan <clark.almazan@gmail.com>
2026-03-24 18:23:35 +0800

    =1?

--- esphome/components/foshan_mppt/foshan_mppt.cpp
+++ esphome/components/foshan_mppt/foshan_mppt.cpp
@@ -114,13 +114,13 @@ void FoshanMPPT::update() {
   if (low_status_byte & (1 << 8)) {
     charge_mode = "Equalizing charge";
   }
-  if (int(high_status_byte) & (1 << 1)) {
+  if ((high_status_byte & (1 << 1)) == 1) {
     charge_mode = "Float charge";
   }
-  if (int(high_status_byte) & (1 << 2)) {
+  if ((int(high_status_byte) & (1 << 2)) == 1) {
     charge_mode = "MPPT charge";
   }
-  if (int(high_status_byte) & (1 << 8)) {
+  if ((int(high_status_byte) & (1 << 8)) == 1) {
     charge_mode = "Standby";
   }
 
